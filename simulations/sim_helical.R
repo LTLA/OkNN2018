0d3e3967a8f979b1a99d8cd79c2fee854162916a
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-21 16:08:10 +0100

    Added simulation scenario for a helical trajectory.

---
+++ simulations/sim_helical.R
@@ -0,0 +1,30 @@
+# Helical trajectory simulation.
+# Calling script should specify 'HEIGHT', 'CYCLES' and 'JITTER'
+
+if (!exists("HEIGHT")) HEIGHT <- 10
+if (!exists("JITTER")) JITTER <- 1 
+if (!exists("CYCLES")) CYCLES <- 1 
+
+source("../functions.R")
+
+FUN <- function(npts, ndim) {
+    total <- pi * CYCLES
+    angle <- runif(npts, 0, total)  
+    x <- cos(angle)
+    y <- sin(angle)
+    z <- angle * HEIGHT / total
+
+    if (ndim==2L) {
+        samples <- cbind(x, y)
+    } else {
+        samples <- cbind(x, y, z)
+        if (ndim > 3L) {
+            samples <- samples %*% matrix(rnorm(3 * ndim), nrow=3) # projected into 'ndim'
+        }
+    }
+
+    samples + rnorm(length(samples), sd=JITTER)
+}
+
+run_simulation(paste0("res_helix-", CYCLES, "-", HEIGHT, "-", JITTER, ".txt"), FUN=FUN)
+
