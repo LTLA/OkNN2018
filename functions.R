a438d44378eb393e82ff1d90e6b1e37ad5c29e6f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-22 10:52:15 +0100

    Expanded suite of NN algorithms for testing.

--- functions.R
+++ functions.R
@@ -8,19 +8,32 @@ find_knn <- function(X, k)
 {
     all.times <- list()
 
+    # FNN-related algorithms.
     gc()
-    all.times$FNN <- system.time(get.knn(X, k))[["elapsed"]]
+    all.times$FNN.kd <- system.time(get.knn(X, k))[["elapsed"]]
 
     gc()
-    all.times$RANN <- system.time(nn2(X, k=k))[["elapsed"]]
+    all.times$FNN.cover <- system.time(get.knn(X, k, algorithm="cover_tree"))[["elapsed"]]
 
+    # RANN-related algorithms.
+    gc()
+    all.times$RANN.kd <- system.time(nn2(X, k=k))[["elapsed"]]
+
+    gc()
+    all.times$RANN.bd <- system.time(nn2(X, k=k, treetype="bd"))[["elapsed"]]
+
+    # KMKNN and variants.
     gc()
     all.times$kmknn <- system.time(findKNN(X, k))[["elapsed"]]
+
+    gc()
+    pre <- precluster(X)
+    all.times$kmknn.pre <- system.time(findKNN(precomputed=pre, k=k))[["elapsed"]]
 
     return(unlist(all.times))
 }    
 
-run_simulation <- function(fname, FUN) 
+run_simulation <- function(fname, FUN, iterations=5) 
 # Function to run a simulation and save the results, 
 # given a function to create the data matrix.
 {
@@ -28,11 +41,13 @@ run_simulation <- function(fname, FUN)
     for (npts in c(5000, 10000, 20000, 50000, 100000)) { 
         for (ndim in c(2, 5, 10, 20, 50)) { 
             for (k in c(2, 5, 10, 20, 50)) {
-                X <- FUN(npts, ndim)
-                out <- find_knn(X, k=k)
-                write.table(data.frame(npts=npts, ndim=ndim, k=k, rbind(out)), file=fname, append=!start, col.names=start, 
-                        row.names=FALSE, quote=FALSE, sep="\t")
-                start <- FALSE
+                for (it in seq_len(iterations)) { 
+                    X <- FUN(npts, ndim)
+                    out <- find_knn(X, k=k)
+                    write.table(data.frame(npts=npts, ndim=ndim, k=k, rbind(out)), file=fname, append=!start, col.names=start, 
+                            row.names=FALSE, quote=FALSE, sep="\t")
+                    start <- FALSE
+                }
             }
         }
     }
